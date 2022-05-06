f5c3ce5b350bc1918bf94da2d10f4fc4b4efe9f8
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2022-05-06 15:32:57 +0300

    comit 5

--- pages/my_measurements.py
+++ pages/my_measurements.py
@@ -1,14 +1,26 @@
 from base_page import BasePage
 from time import sleep
 from selenium.webdriver.common.by import By
+import pyautogui
 
 measurements_button = (By.CLASS_NAME, 'bg-aqua')
 create_measurements_button = (By.CLASS_NAME, 'pull-right')
 category_measurement = (By.CLASS_NAME, 'select2-selection__arrow')
 input_category = (By.CLASS_NAME, 'select2-search__field')
 weight_kg = (By.ID, 'measurement_value')
+time_measurement = (By.ID, 'measurement_create_time')
 save_measurement = (By.ID, 'btn-primary')
-'btn-primary'
+
+#measurement = (By.CLASS_NAME, 'highcharts-markers')
+measurement = (By.XPATH, '//*[@class="highcharts-markers highcharts-series-0 highcharts-tracker"]//*[name()="path"]')
+#measurement =(By.CSS_SELECTOR, '#highcharts-0 > svg > g.highcharts-series-group > g.highcharts-markers.highcharts-series-0.highcharts-tracker > path:nth-child(1)')
+
+created_weight = (By.CLASS_NAME, 'select2-selection__rendered')
+data_1 = (By.CLASS_NAME, 'form-control')
+
+delete_button = (By.XPATH, '//*[@id="measurement-value-form"]/div[2]/a')
+
+'measurement_create_time'
 'self.find_element()'
 
 
@@ -17,13 +29,40 @@ class Measurements (BasePage):
         super().__init__(driver)
         self.driver = driver
 
-    def open_measurements(self):
+    def open__measurements(self):
         self.find_element(measurements_button).click()
 
-    def create_measurements(self):
+    def create_measurements(self, category, data, kg):
         self.find_element(create_measurements_button).click()
         self.find_element(category_measurement).click()
-        self.find_element(input_category).send_keys('Вес')
-        self.find_element(weight_kg).send_keys('80\n')
+        self.find_element(input_category).send_keys(category)
+        self.find_element(time_measurement).clear()
+        self.find_element(time_measurement).send_keys(data)
+        self.find_element(weight_kg).send_keys(kg)
+
         sleep(7)
+
+    def check_measurements(self):
+        sleep(3)
+        self.find_element(measurement).click()
+        self.find_element(measurement).click()
+        return self.find_element(created_weight).text, \
+               self.find_element(weight_kg).get_attribute("value"),\
+               self.find_element(time_measurement).get_attribute("value")
+
+    def redact_measurements(self):
+        sleep(3)
+        self.find_element(measurement).click()
+        self.find_element(measurement).click()
+        self.find_element(weight_kg).click()
+        self.find_element(weight_kg).clear()
+        self.find_element(weight_kg).send_keys('70\n')
+
+        sleep(6)
+
+    def delete_measurements(self):
+        sleep(2)
+        self.find_element(delete_button).click()
+        pyautogui.press('enter')
+        sleep(5)
 
--- tests/test_create_my_measurements.py
+++ tests/test_create_my_measurements.py
@@ -1,6 +1,9 @@
 from base_page import BasePage
 from my_measurements import Measurements
 from home_page import HomePage
+category = 'Вес'
+data = '03.05.2022'
+kg = '80\n'
 
 
 def test_create_measurements(driver):
@@ -10,7 +13,26 @@ def test_create_measurements(driver):
     home_page.open_sign_in()
     base_page.open_my_profile_page()
     measurements = Measurements(driver)
-    measurements.open_measurements()
-    measurements.create_measurements()
+    measurements.open__measurements()
+    measurements.create_measurements(category, data, kg)
+    data_measurements = measurements.check_measurements()
+    measurements.delete_measurements()
+    assert data_measurements == ('Вес', '80', '3.5.2022')
+
+
+def test_redact_measurements(driver):
+    base_page = BasePage(driver)
+    home_page = HomePage(driver)
+    base_page.open_home_page()
+    home_page.open_sign_in()
+    base_page.open_my_profile_page()
+    measurements = Measurements(driver)
+    measurements.open__measurements()
+    measurements.create_measurements(category, data, kg)
+    measurements.redact_measurements()
+    assert True
+
+
+
 
 
